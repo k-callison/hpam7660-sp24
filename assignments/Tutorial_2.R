2ef08db1cb599f9fcf07352d9b8b6c27efba9a8d
k-callison <kcallison@tulane.edu>
2024-01-24 13:52:24 -0600

    Tutorial 2 updates

--- assignments/Tutorial_2.R
+++ assignments/Tutorial_2.R
@@ -1,5 +1,11 @@
+install.packages("nycflights13")
+install.packages("dplyr")
+install.packages("knitr")
+
 library(nycflights13)
 library(dplyr)
 library(knitr)
 
-flights
+ua_delay <- filter(flights, carrier == "UA", year == 2013, month == 1, arr_delay >= 240)
+ua_final <- select(ua_delay, flight, arr_delay)
+kable(ua_final)
